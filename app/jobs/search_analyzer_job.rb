75dab5388945d76fbe4ec962caac23e9bf54ffed
Isabella <isabellaotoo25@gmail.com>
2024-01-05 20:02:20 +0000

    Update search_analyzer_job.rb

--- app/jobs/search_analyzer_job.rb
+++ app/jobs/search_analyzer_job.rb
@@ -2,6 +2,10 @@ class SearchAnalyzerJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+     # Add logic to analyze and summarize searches here
+     popular_queries = Search.group(:query).count.sort_by { |_, count| -count }.take(10)
+
+     # You can store these results in another table or perform any other desired action
+     Trend.create(data: popular_queries.to_h)
   end
 end
